566bfcdc9059e58cc84f30c64bbaa4affc94d8a3
Jeff Linwood <jlinwood@gmail.com>
2023-06-06 14:56:30 -0500

    Add the IP address used on deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -157,5 +157,6 @@ Rails.application.configure do
   # If you need to customize your application's configuration, this is the place to do it. This helps avoid merge
   # conflicts in the future when Rails or Bullet Train update their own default settings.
   config.hosts << ".cloudfront.net"
+  config.hosts << "10.192.11.240"
 
 end
