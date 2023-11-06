a4152e756bb1330a41121b2f0703b22e183b7f73
Jeff Linwood <jlinwood@gmail.com>
2023-11-06 16:52:13 -0600

    Allow any host name for demo deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -156,6 +156,6 @@ Rails.application.configure do
   # ✅ YOUR APPLICATION'S CONFIGURATION
   # If you need to customize your application's configuration, this is the place to do it. This helps avoid merge
   # conflicts in the future when Rails or Bullet Train update their own default settings.
-  config.hosts << ".cloudfront.net"
+  config.hosts = []
   config.host_authorization = { exclude: ->(request) { request.path == '/' } }
 end
